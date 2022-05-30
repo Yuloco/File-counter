a8403a4d452195b88cabd8354d938d1121e7bc81
Yuloco <49050558+Yuloco@users.noreply.github.com>
2022-05-30 03:39:42 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,23 +3,26 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-from cgitb import text
+from posixpath import split
 
 
 def read_file_content(filename):
     # [assignment] Add your code here 
- with open("story.txt", "r") as bread:
-   print(bread.read())
-read_file_content("story,txt")
+    with open ("story.txt") as f:
+        contents = f.read()
+        contents=contents.split(" ")
+        return contents
 
-def count_words():
- text = read_file_content("story.txt")
-    # [assignment] Add your code here
-counts = 0
-for line in text:
- counts += 1
-print (text |": {counts}")
-
-count_words ()
-
-
+def count_words(path):
+    text = read_file_content("story.txt")
+    pack = {}
+    for word in text:
+        if (len(word)) != 0:
+            word = word.strip()
+            word = word.strip ("?/n.,")
+            if word in pack.keys():
+                pack[word] != 1
+            else:
+                pack[word] = 1
+    return (pack)
+print(count_words("story.txt"))
